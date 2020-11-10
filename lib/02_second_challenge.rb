b60266b7718cee607219e2da7c95efc7854f66b7
Miguel  Arreola <marreola4619@gmail.com>
2020-11-10 17:48:48 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,9 +2,17 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :partriarch => {},
+      :martriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
+       :partriarch => {},
+       :martriarch => {},
+       :heroine => {},
+       :heroine_friends => []
       }}
 
   
