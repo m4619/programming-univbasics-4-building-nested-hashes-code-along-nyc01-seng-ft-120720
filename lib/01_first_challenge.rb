9a183552f16471eb7b40a174628b5191bbcdbfb0
Miguel  Arreola <marreola4619@gmail.com>
2020-11-10 17:43:52 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,17 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :partriach => {},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends => []
+     },
+     :capulet => {
+       :partriach => {},
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends => []
+       
+     }
+   }
 end
